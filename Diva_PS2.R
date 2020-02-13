11ebc0aeeedb2162bb0152da3420683827152f51
diva-harsoor <divaharsoor@gmail.com>
2020-02-12 22:39:52 -0600

    comments for days

--- Diva_PS2.R
+++ Diva_PS2.R
@@ -1,10 +1,19 @@
-
-#1)
+---
+title: "4625 PS2"
+author: "Diva Harsoor"
+date: "February 12, 2020"
+output: html_document
+---
+  
+#1) prints out cubes of 1 through 7
 for (val in 1:7) {
   print(val^3)
 }
 
-#2) 
+#2) 1000 simulations of a game 
+# that ends if the first roll is 8,9,10,11,12
+# and if not ends when the dice sum to 2 or 6
+# prints the average number of rolls in each game over 1000 simulations
 set.seed(14)
 num_rolls = 0
 for (i in 1:1000) {
@@ -19,14 +28,17 @@ for (i in 1:1000) {
   }
 }
 avg_rolls = num_rolls/1000
-avg_rolls #4.052
+print(avg_rolls) #4.052
 
-#3
+#3)
 
 #doing this outside the function so we don't have to do it every time
 gss <- read.csv("http://politicaldatascience.com/PDS/Problem%20Sets/Problem%20Set%202/GSS-data.csv")
 gss.df <- as.data.frame(gss)
 
+# function vote.choice takes "Trump", "Clinton", and
+# "Other" as a parameter and returns the corresponding 
+# number of votes from the 2016 election
 vote.choice <- function(candidate) {
   if (candidate != "Trump" && candidate != "Clinton" && candidate != "Other") {
     return("Please enter either ‘Trump’ ‘Clinton’ or ‘Other’ into the function
@@ -38,16 +50,20 @@ to return a valid response")
   }
 }
 
-#4
+#4)
+
+# some infrastructure for the appoint function
 install.packages('fivethirtyeight')
 library(fivethirtyeight)
 turnover.df <- data.frame(cabinet_turnover)
 names <- c("Carter", "Reagan", "Bush 41", "Clinton", "Bush 43", "Obama", "Trump")
 days <- c(1461, 2922, 1461, 2922, 2922, 2922, 1105)
 presidents.df = data.frame(names, days)
-
 subset(presidents.df, names == "Carter")["days"][1,1]
 
+# function appoint takes "Carter", "Reagan", "Bush 41", "Clinton",
+# "Bush 43", and "Obama" as a parameter and returns the avg number 
+# of days appointees served during the corresponding administration
 appoint <- function(pres) {
   current <- na.omit(subset(turnover.df, president == pres)["days"])
   avg <- sum(current)/nrow(current)
@@ -58,9 +74,13 @@ appoint <- function(pres) {
 #5
 age.df <- data.frame(congress_age)
 
+# function congress_stats takes "congress" and "state" as a parameter
+# for congress, it returns the avg age of the 80th to 113th congresses
+# and for state, it returns the avg age of congress members for each of
+# the 50 states
 congress_stats <- function(category) {
   if (category != "congress" && category != "state") {
-    print("This function can only take \"congress\" and \"state\" as arguments. Please try again.")
+    print("This function can only take \"congress\" and \"state\" as its argument Please try again.")
     return -1
   }
   if (category == "congress") {
